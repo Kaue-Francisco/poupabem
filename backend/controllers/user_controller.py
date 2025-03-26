635c78a90f37d64e9feced5346d7f3bea8fa72f7
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-26 15:29:56 -0300

    feat: Implementar métodos de registro de usuário com validação de CPF e verificação de e-mail

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -19,12 +19,73 @@ class UserController:
         """ Método para login de usuário """
 
         return data
-
+    
+    ################################################################
     def register(self, data: dict) -> jsonify:
         """ Método para registro de usuário """
 
-        response = user_service.register(data['name'], data['email'], data['password'], data['cpf'])
-        print(response)
-        return data
+        name, email, password, cpf = data['name'], data['email'], data['password'], data['cpf']
+        
+        if self.exists_email(email) == True:
+            return jsonify({'message': 'Este e-mail já está em uso!'}), 400
+
+        if self.exists_cpf(cpf) == True:
+            return jsonify({'message': 'Este CPF já está em uso!'}), 400
+        
+        if self.validate_cpf(cpf) == False:
+            return jsonify({'message': 'CPF inválido!'}), 400
+
+        response = user_service.register(name=name, email=email, password=password, cpf=cpf)
+
+        return jsonify(response), 201
+    
+    ################################################################
+    def exists_email(self, email: str) -> bool:
+        """ Método para verificar se o email já existe """
+
+        response = user_service.exists_email(email)
+
+        return response
+    
+    ################################################################
+    def validate_cpf(self, cpf: str) -> bool:
+        """ Método para validar CPF """
+        
+        # Remove caracteres não numéricos
+        cpf = ''.join(filter(str.isdigit, cpf))
+        
+        # Verifica se o CPF tem 11 dígitos
+        if len(cpf) != 11:
+            return False
+
+        # Verifica se todos os dígitos são iguais (ex: 111.111.111-11)
+        if cpf == cpf[0] * len(cpf):
+            return False
+
+        # Validação dos dígitos verificadores
+        def calcular_digito(cpf, peso_inicial):
+            soma = sum(int(cpf[i]) * (peso_inicial - i) for i in range(peso_inicial - 1))
+            resto = (soma * 10) % 11
+            return resto if resto < 10 else 0
+
+        # Calcula o primeiro dígito verificador
+        primeiro_digito = calcular_digito(cpf, 10)
+        if primeiro_digito != int(cpf[9]):
+            return False
+
+        # Calcula o segundo dígito verificador
+        segundo_digito = calcular_digito(cpf, 11)
+        if segundo_digito != int(cpf[10]):
+            return False
+
+        return True
+    
+    ################################################################
+    def exists_cpf(self, cpf) -> bool:
+        """ Método para verificar se o CPF já existe """
+
+        response = user_service.exists_cpf(cpf)
+
+        return response
 
 ################################################################
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -12,11 +12,13 @@ class UserService:
     def __init__(self, db_conn: SQLAlchemy):
         self.db_conn = db_conn
 
+    ################################################################
     def login(self, data: dict) -> dict:
         """ Método para login de usuário """
 
         return data
 
+    ################################################################
     def register(self, name: str, email: str, password: str, cpf: str) -> dict:
         """ Método para registro de usuário """
 
@@ -28,3 +30,25 @@ class UserService:
             return {'error': str(e)}
 
         return {'message': 'Usuário cadastrado com sucesso!'}
+    
+    ################################################################
+    def exists_email(self, email: str) -> bool:
+        """ Método para verificar se o email já existe """
+
+        user = self.db_conn.session.query(User).filter_by(email=email).first()
+
+        if user:
+            return True
+        
+        return False
+    
+    ################################################################
+    def exists_cpf(self, cpf: str) -> bool:
+        """ Método para verificar se o CPF já existe """
+
+        user = self.db_conn.session.query(User).filter_by(cpf=cpf).first()
+
+        if user:
+            return True
+        
+        return False
